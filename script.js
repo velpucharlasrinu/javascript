43fb66c295d4b5f56df749820b7736bbd5fca6f2
velpucharlasrinu <velpucharlasrinu555@gmail.com>
2022-06-22 08:44:30 +0530

    js-practice

---
+++ script.js
@@ -0,0 +1,54 @@
+// string 
+var intro= "srinu .v";
+console.log(typeof intro , intro);
+
+// number
+var a = 10;
+console.log(typeof a, a);
+
+// boolean
+var conf = true;
+console.log(typeof conf,conf);
+
+// null
+var object = null;
+console.log(typeof object,object);
+
+// undefined 
+var prog = undefined;
+console.log(typeof prog ,prog)
+
+// symbol
+var texture =Symbol();
+console.log(typeof texture,texture)
+
+// corceian
+var Name = "sai";
+var  a = 10;
+var sum = Name+a;
+console.log(typeof sum,sum);
+
+// calculations
+// add
+var a = 20 ;
+var b = 10;
+var sum = a+b;
+console.log( sum );
+
+// sub
+var a = 20;
+var b = 10;
+var sub = a-b;
+console.log(sub);
+
+// mul
+var a = 20;
+var b = 10;
+var mul = a*b;
+console.log(mul);
+
+// division
+var a = 20;
+var b = 10;
+var div = a/b;
+console.log(div);
