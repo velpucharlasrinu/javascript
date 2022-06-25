f8937aa4a23c554616e0ec8de1e57ee12a0939b2
velpucharlasrinu <velpucharlasrinu555@gmail.com>
2022-06-25 18:51:24 +0530

    second-session

--- script.js
+++ script.js
@@ -52,3 +52,135 @@ var a = 20;
 var b = 10;
 var div = a/b;
 console.log(div);
+
+
+// conditional statements
+ 
+//if condition
+ var a = true;
+ if (a){
+    console.log("it's true statement");
+ }
+
+ //if, else condition
+ var a = 20;
+ var b = 10;
+ if (a<b){
+    console.log("true statement");
+ }
+else{
+    console.log("false statement");
+}
+ 
+// if,else if,else
+
+var a = 10;
+var b = 20;
+var c = 30;
+if (c<b) {
+    console.log("this is true");
+}
+else if (a+b) {
+    console.log("sum is equal to a");
+} 
+else{
+        console.log("this is false");
+}
+
+//  operations
+ //  arithematic operations
+ //addition (+)
+ var a = 19;
+ var b = 1;
+ console.log(a+b);
+
+//sub (-)
+ var a = 19;
+ var b = 5;
+ console.log(a-b);
+
+// multiplication(*)
+ var a = 10;
+ var b = 5;
+ console.log(a*b);
+
+ //division(/)
+var a = 10;
+var b = 2;
+console.log(a/b);
+
+//remainder of division (%)
+var a = 5;
+var b = 30;
+console.log(a%b);
+
+//increment (++)
+// pre increment
+var a = 10;
+console.log(++a);
+
+//post increment(++)
+var a = 10;
+console.log(a++);
+console.log(a);
+
+// decrement (--)
+//pre decrement
+var a = 10;
+console.log(--a);
+
+//post decrement
+var a = 10;
+console.log(a--);
+console.log(a);
+
+//comperison operator
+// 1. logical operator 
+// a. (&&)and operator
+ var a = 10;
+ var b = 5;
+ console.log(a < 20 && b < 10);
+//b.(||) or operator
+   var a = 10;
+   var b = 15;
+   console.log(a < 15 || b < 5 );
+//c.(!) not operator
+var a = 10 ;
+var b = 15 ;
+console.log(!(a==b));  
+ 
+
+// 2. relational operator
+// <,>,<=,>=
+ var a = 10;
+ var b = 15;
+ console.log(a<b);
+
+ var a = 20;
+ var b = 15;
+ console.log(a>b);
+ 
+ var a = 10;
+ var b = 10;
+ console.log(a<=b);
+ 
+ var a = 10;
+ var b = 9;
+ console.log(a>=b);
+
+ //3. equality operator
+ // 1. lose equality (==) 
+  var a = 5;
+  var b = 20;
+  console.log(a==b);
+
+// 2. strict equality(===) 
+   var a ="string";
+   var b = 5;
+   console.log(a===b);
+
+//4. conditional operator
+var score = 90;
+    score > 80 ? 
+    console.log("allowed to interview");
+    console.log("not allowed to interwiew");
